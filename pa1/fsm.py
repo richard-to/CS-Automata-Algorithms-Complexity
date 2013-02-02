f03ea6ccc1e1aea15a85ce7552385585a4eafaaf
richard-to <gohn76@gmail.com>
2013-02-02 13:28:13 -0900

    Add docstring for fsm function

--- pa1/fsm.py
+++ pa1/fsm.py
@@ -1,3 +1,47 @@
+"""
+Finite state machine implementation for DFA's.
+
+This FSM cheats a bit in that error states are implicit. This 
+means that if a transition is not in the state, then this is counted
+as an error state.
+
+Args:
+	string: A string to check as valid for a given DFA
+	state: The starting state for the given DFA
+	dfa: See examples for DFA structure
+	final_states: A tuple of accepting states
+
+Returns:
+	True if the string is a valid for the given DFA.
+	False if the string is invalid.
+
+Example Usage:
+	
+	string = '00111'
+
+	start_state = 'A'
+
+	dfa = {
+		'A': (
+			('0', 'A'),
+			('1', 'B'),
+		),
+		'B': (
+			('0', 'A'),
+			('1', 'C')
+		),
+		'C': (
+			('0', 'D'),
+			('1', 'C')
+		),
+		'D': (
+			(('0', '1'), 'D'),
+		),																
+	}
+
+	final_states = ('C')
+"""
+
 def fsm(string, state, dfa, final_states):	
 	if len(string) == 0:
 		return state in final_states
